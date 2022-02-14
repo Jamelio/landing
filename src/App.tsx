0f94ba1cedc080687660b63235cfc9da33f631b8
Sergei Basharov <sergei.basharov@unagi.ch>
2022-02-14 20:09:27 +0300

    Added global style, cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Viewport } from './Viewport/Viewport';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: black;
+    padding: 0;
+    margin: 0;
+  }
+`
 
 export default function App() {
-  return <Viewport />
+  return <>
+    <GlobalStyle />
+    <Viewport />
+  </>
 }
