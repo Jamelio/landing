2926dfec0fa2b617d3f616bc0e30aab745bd633e
Sergei Basharov <sergei.basharov@unagi.ch>
2022-02-15 18:57:54 +0300

    Separated landing and client app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# landing
+# Jamelio Landing
--- src/Viewport/Viewport.styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const MainContainer = styled.div`
-  background-color: gray;
-  display: flex;
-  flex-direction: column;
-`
